6ba00006b97e6208c58a3a28674cb8c736425f0b
ehsan2793 <ehsan2793@gmail.com>
2022-03-01 22:28:08 -0800

    miles converter works

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
-
+def miles_to_km():
+    miles = miles_input.get()
+    km = int(miles) * 1.609
+    kilometer.config(text=km)
 window = Tk()
 window.title("Miles to Kilometer Converter")
-window.minsize(width=500,height=500)
-window.config(padx=20 , pady=20)
+# window.minsize(width=500,height=500)
+window.config(padx=50 , pady=20)
 
-miles_input = Entry()
+miles_input = Entry(width=5 )
 miles_input.grid(column=1,row=0)
 
 miles_label = Label(text="Miles")
@@ -20,7 +23,8 @@ kilometer.grid(column=1,row=1)
 kilometer_unit = Label(text="Km")
 kilometer_unit.grid(column=2,row=1)
 
-calculate = Button(text="Calculate")
+calculate = Button(text="Calculate",command=miles_to_km)
 calculate.grid(column=1,row=2)
+
 
 window.mainloop()
