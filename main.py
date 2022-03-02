1140a1bbbab9d8f229c69d3f48a2e072adf31059
ehsan2793 <ehsan2793@gmail.com>
2022-03-01 22:23:47 -0800

    layout added

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from tkinter import *
 
 window = Tk()
 window.title("Miles to Kilometer Converter")
+window.minsize(width=500,height=500)
+window.config(padx=20 , pady=20)
 
+miles_input = Entry()
+miles_input.grid(column=1,row=0)
 
+miles_label = Label(text="Miles")
+miles_label.grid(column=2,row=0)
 
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0,row=1)
 
+kilometer = Label(text="0")
+kilometer.grid(column=1,row=1)
+
+kilometer_unit = Label(text="Km")
+kilometer_unit.grid(column=2,row=1)
+
+calculate = Button(text="Calculate")
+calculate.grid(column=1,row=2)
 
 window.mainloop()
